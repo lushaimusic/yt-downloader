a42ca9a3e008a9c8e13a44197e3a196be672686d
zauteakm <77600757+zauteakm@users.noreply.github.com>
2021-03-09 20:31:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Support Channel", url="https://t.me/ZKbotsUpdate")],
         [InlineKeyboardButton(
-            "Support Group👥", url="https://t.me/MizoInFoTel1")]
+            "Support Group", url="https://t.me/MizoInFoTel1")]
     ])
     welcomed = f"Hi! <b>{message.from_user.first_name}</b>\n/help lo thawn la aw, Min hmandan tur i hriat duh chuan."
     await message.reply_text(welcomed, reply_markup=joinButton)
